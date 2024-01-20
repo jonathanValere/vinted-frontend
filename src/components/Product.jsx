12efcfdaca3cb3c68f79af79cf187193cc378d90
“Jonathan <jonathan.valere@gmail.com>
2024-01-20 13:05:49 -0300

    CSS product added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,20 +4,24 @@ import styles from "./Product.module.css";
 export default function Product({ ...props }) {
   return (
     <article className={styles.product}>
-      <div>
+      <div className={styles.username}>
         {props.offer.owner.account.avatar && (
           <img
             src={props.offer.owner.account.avatar.secure_url}
             alt="avatar"
-            width={20}
-            height={20}
+            width={30}
+            height={30}
           />
         )}
         {props.offer.owner.account.username}
       </div>
-      <img src={props.offer.product_image.secure_url} alt="image du produit" />
-      <div>
-        <p>{props.offer.product_price} €</p>
+      <img
+        src={props.offer.product_image.secure_url}
+        alt="image du produit"
+        className={styles["product-image"]}
+      />
+      <div className={styles["product-infos"]}>
+        <p className={styles["product-price"]}>{props.offer.product_price} €</p>
         {props.offer.product_details.map((el, index) => (
           <div key={index}>
             <p>{el.TAILLE}</p>
