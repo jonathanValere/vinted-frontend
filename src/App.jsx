746cca89a9df62e4298bc73cd80efa5dd847f913
“Jonathan <jonathan.valere@gmail.com>
2024-01-20 09:34:11 -0300

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
 
 function App() {
-  return <h1>Frontend vinted</h1>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
