e2122bb811bfd2bf1eea536390c72466c0822af1
“Jonathan <jonathan.valere@gmail.com>
2024-01-20 10:28:21 -0300

    fetchData with axios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 export default function Header() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/offer">Offer</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/offer">Offer</Link>
+          </li>
+        </ul>
+      </nav>
+      <Button title="S'inscrire" />
+      <Button title="Se connecter" />
+      <Button title="Vends tes articles" />
+    </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Home() {
-  return <p>homepage</p>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  return isLoading ? (
+    <p>En chargement...</p>
+  ) : (
+    <main>
+      {data.offers.map((offer) => (
+        <article key={offer._id}>
+          <div>
+            {console.log(offer.owner.account.avatar.secure_url)}
+            <img
+              src={offer.owner.account.avatar.secure_url}
+              alt="avatar"
+            />{" "}
+            {offer.owner.account.username}
+          </div>
+        </article>
+      ))}
+    </main>
+  );
 }
