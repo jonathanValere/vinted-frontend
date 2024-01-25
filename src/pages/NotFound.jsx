3297bb8e6338846784b17b8b0c88ad5ba54b1544
“Jonathan <jonathan.valere@gmail.com>
2024-01-25 15:42:57 -0300

    page 404 modified

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,11 @@ export default function NotFound() {
     <section>
       <div className="container">
         <div className={styles["not-found"]}>
-          <p>Page non trouvée...</p>
+          <p>La page n'existe pas...</p>
+          <p>
+            Désolé, mais on dirait que cette page n'existe plus. Pourquoi ne pas
+            revenir en arrière et essayer autre chose ?
+          </p>
           <Link to="/">Retour à la page d'accueil</Link>
         </div>
       </div>
